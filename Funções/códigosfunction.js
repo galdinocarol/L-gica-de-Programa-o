b9957e2a94ffdd47c089f31bb3e558057af8777d
Caroline <caroline.galdinobonfim@gmail.com>
2025-05-27 11:25:03 -0300

    função 2° att

---
+++ Funções/códigosfunction.js
@@ -0,0 +1,78 @@
+console.log("Faça seu login")
+let digitelogin1 = prompt("Digite seu usuario:")
+let digitelogin2 = prompt("Digite sua senha:")
+
+let usuario = "Caroline Galdino"
+let senha = "1234"
+
+let usuario1 = "Gatinha"
+let senha1 = "miau"
+
+let usuario2 = "Cachorrinho"
+let senha2 = "auau"
+
+let usuario3 = "vaquinha"
+let senha3 = "mumu"
+
+let usuario4 = "porquinho"
+let senha4 = "onconc"
+
+let usuario5 = "grilo"
+let senha5 = "cricri"
+
+let usuario6 = "pato"
+let senha6 = "quaqua"
+
+let usuario7 = "pombo"
+let senha7 = "grugru"
+
+let usuario8 = "macaquinho"
+let senha8 = "uaa"
+
+let usuario9 = "leão"
+let senha9 = "ruar"
+
+
+
+function verificarlogin(usuario, senha){
+
+if (digitelogin1 === usuario && digitelogin2 === senha){
+console.log("Login válido!");
+}
+
+else if (digitelogin1 === usuario1 && digitelogin2 === senha1){
+console.log("Login válido!");}
+
+else if (digitelogin1 === usuario2 && digitelogin2 === senha2){
+console.log("Login válido!");}
+
+else if (digitelogin1 === usuario3 && digitelogin2 === senha3){
+console.log("Login válido!");}
+
+else if (digitelogin1 === usuario4 && digitelogin2 === senha4){
+console.log("Login válido!");}
+
+else if (digitelogin1 === usuario5 && digitelogin2 === senha5){
+console.log("Login válido!");}
+
+else if (digitelogin1 === usuario6 && digitelogin2 === senha6){
+console.log("Login válido!");}
+
+else if (digitelogin1 === usuario7 && digitelogin2 === senha7){
+console.log("Login válido!");}
+
+else if (digitelogin1 === usuario8 && digitelogin2 === senha8){
+console.log("Login válido!");}
+
+else if (digitelogin1 === usuario9 && digitelogin2 === senha9){
+
+console.log("Login válido!");
+}
+
+else{
+console.log("Login inválido!");
+}
+}
+
+verificarlogin(usuario,senha,usuario1,senha1,usuario2,senha2,usuario3,senha3,usuario4,senha4,usuario5,senha5,usuario6,senha6,usuario7,senha7,usuario8,senha8,usuario9,senha9)
+
