26c1ce699de9890497fe3503f0689994e2f453a1
Caroline <caroline.galdinobonfim@gmail.com>
2025-06-03 11:03:15 -0300

    Exercicios 1 - Funções Tradicionais JS

---
+++ Funções/Exercicios1-FunçoesTradicionaisJS.js
@@ -0,0 +1,139 @@
+1
+function saudacao(){
+    console.log("Olá mundo!")
+}
+saudacao()
+
+2
+function mostrarNome(){
+    console.log("Caroline Galdino")
+}
+mostrarNome()
+
+3
+function mostrarIdade(){
+    console.log(17)
+}
+mostrarIdade()
+
+4
+let num1 = 44
+let num2 = 256
+
+function somarDoisNumeros(){
+    resultado = num1 + num2
+    return resultado
+}
+console.log("O resultado é",somarDoisNumeros())
+
+5
+function mostrarMensagem(){
+    console.log("Menos foco, mais ansiedade!")
+}
+mostrarMensagem()
+
+6
+function mostrarData(){
+    console.log("Hoje é dia: 06/05/2025")
+}
+mostrarData()
+
+7 
+function mostrarCidade(){
+    console.log("A cidade local é Ferraz de Vasconcelos")
+}
+mostrarCidade()
+
+8
+function nome(){
+let nome = prompt("Digite seu nome: ")
+return nome
+}
+console.log("Bem vindo", nome())
+
+9
+let num1 = Number(prompt("Digite o primeiro numero: "))
+let num2 = Number(prompt("Digite o segundo numero: "))
+
+function subtracao(){
+    resultado = num1 - num2
+    return resultado 
+}
+console.log("O resultado é",subtracao())
+ 10
+function meuAnimalFavorito(){
+    animal = "macaquinho"
+    console.log("Meu animal favorito é um",animal)
+}
+meuAnimalFavorito()
+
+11
+function curso(){
+    console.log("Estou aprendendo JavaScript!")
+}
+curso()
+
+12
+let num1 = Number(prompt("Digite um numero: "))
+
+function multiplicacao(){
+    resultado = num1 * 3
+    return resultado
+}
+console.log("O tripo desse numero é",multiplicacao())
+
+13 
+function informacoes(){
+    let nome = prompt("Qual é o seu nome? ")
+    let idade = Number(prompt("Qual é a sua idade? "))
+    console.log("Voçê se chama", nome , "e tem", idade, "anos de idade.")
+}
+informacoes()
+
+14
+function numeros(){
+    console.log(1,2,3,4,5,6,7,8,9, 10)
+}
+numeros()
+
+15 
+function aula(){
+    console.log("Aula de funções iniciada!")
+}
+aula()
+
+16
+function number(){
+    let numero = Number(prompt("Digite um numero: "))
+    console.log ("Voçê digitou o numero",numero)
+}
+number()
+
+17
+function cursos(){
+    console.log("Análise e Desenvolvimento de Sistemas")
+}
+cursos()
+
+18
+function chamada(){
+    let nome = prompt("Qual seu nome? ")
+    console.log(nome, ",dirija-se ao consutório 5, para ressonancia.")
+}
+chamada()
+
+19
+function corFavorita(){
+    let corFavorita = prompt("Digite sua cor favorita: ")
+    console.log("Sua cor favorita é", corFavorita)
+}
+corFavorita()
+
+20
+function pais(){
+    let pais = prompt("Qual o seu País? ")
+    console.log("Seu país é",pais)
+}
+pais()
+
+
